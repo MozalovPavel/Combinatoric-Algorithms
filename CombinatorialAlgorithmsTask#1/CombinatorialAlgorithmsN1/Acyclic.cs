cc8615d847e4b2a641ef92a82d7e17c51eb82490
kreolim <suspect91717@gmail.com>
2015-04-06 15:43:59 +0500

    Total code cleanup

--- CombinatorialAlgorithmsTask#1/CombinatorialAlgorithmsN1/Acyclic.cs
+++ CombinatorialAlgorithmsTask#1/CombinatorialAlgorithmsN1/Acyclic.cs
@@ -1,65 +1,50 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using System.Linq;
-using System.Text;
 using System.IO;
-using System.Threading.Tasks;
 
 namespace CombinatorialAlgorithmsN1
 {
     public class Acyclic
     {
-        public Acyclic (string address)
+        public Acyclic(string address)
         {
-            
             string[] file = File.ReadAllLines(address);
-            var lengthFile =file.GetLength(0);
-            for(var i =0; i < lengthFile;i++)
+            var lengthFile = file.GetLength(0);
+            for (var i = 0; i < lengthFile; i++)
             {
-                if (i == 0) {
-                    
-                    n = int.Parse(file[i].ToString());
-                    father= new int[n];
-                    //Console.WriteLine(n);
+                if (i == 0)
+                {
+                    int n = int.Parse(file[i]);
+                    father = new int[n];
                 }
                 else
                 {
-                    var str = file[i].Split (new  char[]{' '});
-                    var list = new List<int>();
-                    foreach (var j in str)
-                    {
-                        var l =  int.Parse(j.ToString());
-                        if (l != 0)
-                        {
-                            //Console.WriteLine(l);
-                            list.Add(l);
-                        }   
-                    }
-                graph.Add(list);
+                    var str = file[i].Split(new[] {' '});
+                    var list = str.Select(int.Parse).Where(l => l != 0).ToList();
+                    graph.Add(list);
                 }
             }
         }
-        public List<List<int>> graph = new List<List<int>>();
-        public List<int> visited = new List<int>();
-        public int[] father;
-        public int n;
+
+        private readonly List<List<int>> graph = new List<List<int>>();
+        private readonly List<int> visited = new List<int>();
+        private readonly int[] father;
+
         public void Result()
         {
             var c = new string[2];
             for (var i = 1; i <= graph.Count; i++)
             {
-                if (!visited.Contains(i))
-                {
-
-                    var b =bfs(i);
-                    c = b;
-                    if (b[0] != "A")
-                        break;
-                }
+                if (visited.Contains(i)) continue;
+                var b = BFS(i);
+                c = b;
+                if (b[0] != "A")
+                    break;
             }
-            File.WriteAllLines( "out.txt", c );
+            File.WriteAllLines("out.txt", c);
         }
-        public string[] bfs(int i)
+
+        private string[] BFS(int i)
         {
             var stack = new Stack<int>();
             var queue = new Queue<int>();
@@ -68,28 +53,25 @@ namespace CombinatorialAlgorithmsN1
             {
                 var j = queue.Dequeue();
                 visited.Add(j);
-                var notQueueFromNotFather = new List<int>();
-                notQueueFromNotFather = graph[j - 1].Where(x => x != father[j - 1]).ToList()
-                    .Where(x => !queue.Contains(x)).ToList()
-                    .Where(x => visited.Contains(x)).ToList();
+                List<int> notQueueFromNotFather = graph[j - 1]
+                    .Where(x => x != father[j - 1])
+                    .Where(x => !queue.Contains(x))
+                    .Where(x => visited.Contains(x))
+                    .ToList();
                 if (notQueueFromNotFather.Count == 0)
                 {
                     foreach (var e in graph[j - 1])
                     {
                         if (!visited.Contains(e))
                         {
-                            //Console.WriteLine(e);
                             father[e - 1] = j;
                             if (!queue.Contains(e))
                             {
-                                //Console.WriteLine(e);
                                 queue.Enqueue(e);
                             }
                         }
                     }
                     stack.Push(j);
-                    
-                    //Console.WriteLine(stack.Count);
                 }
                 else
                 {
@@ -103,20 +85,11 @@ namespace CombinatorialAlgorithmsN1
                     str.Add(s);
                     str.Add(j);
                     str.Sort();
-                    var str2 = "";
-                    foreach (var l in str)
-                        str2 += l.ToString() + " ";
-                    return new string[] { "N", str2};
+                    var str2 = str.Aggregate("", (current, l) => current + (l + " "));
+                    return new[] {"N", str2};
                 }
-                    //return new string[] { "N", StackToString(stack, j) };
-
             }
-            return new string[] { "A" };
+            return new[] {"A"};
         }
-        //public string StackToString(Stack<int> stack, int j)
-        //{
-            
-        //}
-
     }
 }
